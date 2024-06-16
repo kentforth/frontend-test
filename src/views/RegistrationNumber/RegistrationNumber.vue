fb7972a7eeba76a21565542a1b1f84a0fddf34b8
Konstantin Rusanov <rusanov@uteka.ru>
2024-06-16 22:02:36 +0700

    Add sending email in registration number

--- src/views/RegistrationNumber/RegistrationNumber.vue
+++ src/views/RegistrationNumber/RegistrationNumber.vue
@@ -8,11 +8,14 @@ export default {
 import {query, collection, getDocs} from 'firebase/firestore'
 import {db} from '@/services/firebase'
 
+import emailjs from 'emailjs-com';
+
 const route = useRoute()
 
 const number = ref(null)
 const riders = ref([])
 const isPageLoaded = ref(false)
+const emailTo = ref(null)
 
 const newRegistration = ref(route.query.newRegistration)
 
@@ -38,11 +41,31 @@ const getNoun = (number, one, two, five) => {
   return five;
 }
 
-const sendEmail = () => {
+const onClick = () => {
   sentEmails += 1
   if (sentEmails <= 1) {
     console.log('SEND EMAIL')
+    // sendEmail(number.value, emailTo.value)
   }
+}
+
+const sendEmail = (number, email) => {
+  const templateParams = {
+    to: email,
+    subject: 'Регистрация на Урочище 2024',
+    message: `
+    Ты зарегистрирован(а) на гонку Урочище 2024.<br/>
+    Номер участника: ${number} <br/>
+    Даты: 2024.07.19 - 2024.07.21 <br/>
+    Место: 56.373234 84.962397 <br/>
+    Стоимость для участников: $ <br/>
+    Реквизиты для оплаты: [номер карты/банк/qr] <br/>
+    <span style="text-decoration: underline">при оплате указывай номер участника в комментарии к платежу</span> <br/>
+    По всем вопросам: +7 (3822) 77-99-10
+<!--    Оплатить: <img src="https://s541vla.storage.yandex.net/rdisk/7c2190e5352de99e1d4e70f50a17b8bf5d7027fa85b20f46cd4b7e06fd6d3fec/6665ea99/4Vt41AMDvZ9eyQgEKS_sYSPTxuhfHm3rIG4SVnVJPZ212S3yX4s_tJk_JjTXOHLmwY2z4DL9gF6whwAmq_d88g==?uid=398863231&filename=payment.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=398863231&fsize=2501&hid=7f9e356169ff01400788b0f7985e0871&media_type=image&tknv=v2&etag=2c5a79532a9b8bbb102273b2541f0833&ts=61a789eed1840&s=521ab0904af30e988fe993920cd070e38e22606115444c5af154af5ce76a9aab&pb=U2FsdGVkX18JbEL-jl1A9oO8e4NocOnZa41y5JO45NDn2QlTpC5WNGoIiwfBkLcgFIRPe9vcEFpN7OtVGXD9BGnsA-esqGXXZYDlA11y5Lo" alt="qr" width="200" height="200">-->
+    `
+  }
+  emailjs.send('service_7nte8nd', 'urochishe2024', templateParams, 'user_Q62B7RRlfcIcFCktKXEDM')
 }
 
 onBeforeMount(async () => {
@@ -68,6 +91,7 @@ onBeforeMount(async () => {
 
   if (foundEmail) {
     number.value = foundEmail.number
+    emailTo.value = foundEmail.email
   }
 
   isPageLoaded.value = true
@@ -93,7 +117,7 @@ onBeforeMount(async () => {
     </div>
 
     <p class="registration-number__email" v-if="newRegistration">на адрес твоей электронной почты отправлены реквизиты для оплаты и вся необходимая информация</p>
-    <button v-else class="registration-number__email" @click="sendEmail">отправить информацию повторно</button>
+    <button v-else class="registration-number__email" @click="onClick">отправить информацию повторно</button>
 
     <div class="registration-number__links">
       <RouterLink :to="{ name: 'home' }">главная</RouterLink>
