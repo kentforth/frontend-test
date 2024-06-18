90a27bd0ad3c18b7b7ef458e9bd7cea83a563b52
Konstantin Rusanov <rusanov@uteka.ru>
2024-06-18 10:41:17 +0700

    Add try catch to registration number

--- src/views/RegistrationNumber/RegistrationNumber.vue
+++ src/views/RegistrationNumber/RegistrationNumber.vue
@@ -71,8 +71,13 @@ const sendEmail = (number, email) => {
 <!--    Оплатить: <img src="https://s541vla.storage.yandex.net/rdisk/7c2190e5352de99e1d4e70f50a17b8bf5d7027fa85b20f46cd4b7e06fd6d3fec/6665ea99/4Vt41AMDvZ9eyQgEKS_sYSPTxuhfHm3rIG4SVnVJPZ212S3yX4s_tJk_JjTXOHLmwY2z4DL9gF6whwAmq_d88g==?uid=398863231&filename=payment.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=398863231&fsize=2501&hid=7f9e356169ff01400788b0f7985e0871&media_type=image&tknv=v2&etag=2c5a79532a9b8bbb102273b2541f0833&ts=61a789eed1840&s=521ab0904af30e988fe993920cd070e38e22606115444c5af154af5ce76a9aab&pb=U2FsdGVkX18JbEL-jl1A9oO8e4NocOnZa41y5JO45NDn2QlTpC5WNGoIiwfBkLcgFIRPe9vcEFpN7OtVGXD9BGnsA-esqGXXZYDlA11y5Lo" alt="qr" width="200" height="200">-->
     `
   }
-  
-  emailjs.send('service_7nte8nd', 'urochishe2024', templateParams, 'user_Q62B7RRlfcIcFCktKXEDM')
+
+  try {
+    emailjs.send('service_7nte8nd', 'urochishe2024', templateParams, 'user_Q62B7RRlfcIcFCktKXEDM')
+  } catch (e) {
+    console.error(e)
+  }
+
 }
 
 onBeforeMount(async () => {
