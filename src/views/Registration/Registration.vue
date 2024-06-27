f67e3f7f6df617a54354a80e09c7fe916c99e4ec
Konstantin Rusanov <rusanov@uteka.ru>
2024-06-27 23:11:25 +0700

    Add from to email

--- src/views/Registration/Registration.vue
+++ src/views/Registration/Registration.vue
@@ -151,6 +151,7 @@ const onSubmit = async () => {
 
       const templateParams = {
         to: form.value.email,
+        from: 'urochisherace@gmail.com',
         message: {
           subject: 'Регистрация на гонку Урочище 2024',
           html: `
