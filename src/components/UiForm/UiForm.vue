46ba03869fa9731e7abb7785e78f86095f9412b3
Konstantin Rusanov <rusanov@uteka.ru>
2024-12-04 17:52:28 +0700

    Add form validation

--- src/components/UiForm/UiForm.vue
+++ src/components/UiForm/UiForm.vue
@@ -10,7 +10,44 @@ import { useAccountStore } from '@/store/account'
 
 const labels = ['Метки', 'Тип записи', 'Логин', 'Пароль']
 
+const form = ref(false)
+const login = ref(null)
+const password = ref(null)
+
 const accountStore = useAccountStore()
+
+const updateSelect = (index: number) => {
+  accountStore.updateAccountType(index)
+  validateForm()
+}
+
+const resetLoginValidation = (index: number) => {
+  if (
+    accountStore.accounts[index][2].value === null ||
+    accountStore.accounts[index][2].value === undefined
+  ) {
+    login.value[0].reset()
+  }
+}
+
+const resetPasswordValidation = async (index: number) => {
+  if (
+    accountStore.accounts[index][3].value === null ||
+    accountStore.accounts[index][3].value === undefined
+  ) {
+    password.value[0].reset()
+  }
+}
+
+const validateForm = () => {
+  if (!form.value.isValid) {
+    form.value.validate()
+
+    return
+  }
+
+  console.log('VALID')
+}
 </script>
 
 <template>
@@ -30,36 +67,51 @@ const accountStore = useAccountStore()
         <span v-for="label in labels" :key="label">{{ label }}</span>
       </div>
 
-      <v-form class="form__form">
+      <v-form class="form__form" ref="form">
         <template v-for="(field, index) in accountStore.accounts" :key="field[0].id">
           <v-text-field
+            v-model="accountStore.accounts[index][0].value"
             variant="outlined"
             maxLength="50"
-            v-model="accountStore.accounts[index][0].value"
+            hide-details
+            @input="validateForm"
           />
 
           <v-select
             v-model="accountStore.accounts[index][1].value"
             :items="accountStore.items"
             variant="outlined"
-            @update:modelValue="accountStore.updateAccountType(index)"
+            hide-details
+            @update:modelValue="updateSelect(index)"
           />
 
           <v-text-field
+            ref="login"
             v-model="accountStore.accounts[index][2].value"
             variant="outlined"
             maxLength="100"
+            required
+            :rules="[(v) => !!v || 'Поле обязательно']"
+            hide-details
             class="w-100"
             :class="accountStore.accounts[index][1].value === 'ldap' ? 'form__login-field' : ''"
+            @blur="resetLoginValidation(index)"
+            @input="validateForm"
           />
 
           <v-text-field
+            ref="password"
             v-if="accountStore.accounts[index][1].value === 'local'"
             v-model="accountStore.accounts[index][3].value"
             type="password"
             variant="outlined"
             maxLength="100"
+            hide-details
+            :rules="[(v) => !!v || 'Поле обязательно']"
+            :required="accountStore.accounts[index][1].value === 'local'"
             append-inner-icon="mdi-eye-off-outline"
+            @blur="resetPasswordValidation(index)"
+            @input="validateForm"
           />
 
           <button class="form__btn-delete" @click.prevent="accountStore.deleteAccount(index)">
