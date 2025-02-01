c9ff64a020fe9427c73755ab7397a388b7e99830
kentforth <kentforth@mail.ru>
2025-02-01 13:09:00 +0700

    Create sidebar

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="sidebar">
+    <div class="sidebar__header">sdfsdf</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.sidebar {
+  width: max-content;
+  height: 100svh;
+  background: $white;
+  box-shadow:
+    rgba(14, 30, 37, 0.12) 0 0 4px 0,
+    rgba(14, 30, 37, 0.32) 0 0 16px 0;
+
+  &__header {
+    background: $lightblue;
+    padding: 16px;
+  }
+}
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -4,9 +4,12 @@ export default {
 }
 </script>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Sidebar from '@/components/Sidebar/Sidebar.vue'
+</script>
 
 <template>
   <div class="home">
+    <Sidebar/>
   </div>
 </template>
