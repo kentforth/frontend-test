28bba02d8272757719128b51ae8423e5040b20a5
kentforth <kentforth@mail.ru>
2025-02-03 12:06:49 +0700

    Add fold button to sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import UiSearch from "@/components/UiSearch/UiSearch.vue"
 import type { IUser } from "@/types"
+import arrow from "@/assets/icons/chevron-right.svg"
 
 const emit = defineEmits([
   "get-user",
@@ -23,6 +24,8 @@ const {
   activeUserId = null,
 } = defineProps<IProps>()
 
+const isContentVisible = ref(false)
+
 const setActiveTab = (tab: string) => {
   emit("set-active-tab", tab)
 }
@@ -34,40 +37,59 @@ const searchUser = (user: string) => {
 
 <template>
   <div class="sidebar">
-    <div class="sidebar__header">
-      <ButtonSwitcher
-        text-left="Clients"
-        text-right="Rating"
-        @click="setActiveTab"
-      />
-
-      <UiSearch
-        class="sidebar__search"
-        @search="searchUser"
-      />
-    </div>
-
-    <p
-      v-if="error"
-      class="sidebar__error"
+    <button
+      class="sidebar__btn-fold"
+      :class="!isContentVisible ? 'sidebar__btn-fold--folded' : ''"
+      @click="isContentVisible = !isContentVisible"
     >
-      {{ error }}
-    </p>
-
-    <UserList
-      :users="users"
-      :active-user-id="activeUserId"
-      :is-rating-tab="isRatingTab"
-      @get-user="$emit('get-user', $event)"
-    />
-
-    <div class="sidebar__footer">
-      <div></div>
-      <UiButton
-        title="Update list"
-        @click="$emit('update-users')"
+      <img
+        :src="arrow"
+        alt="arrow"
       />
+    </button>
+    <div
+      class="sidebar__content"
+      v-if="isContentVisible"
+    >
+      <div class="sidebar__header">
+        <ButtonSwitcher
+          text-left="Clients"
+          text-right="Rating"
+          @click="setActiveTab"
+        />
+
+        <UiSearch
+          class="sidebar__search"
+          @search="searchUser"
+        />
+      </div>
+
+      <p
+        v-if="error"
+        class="sidebar__error"
+      >
+        {{ error }}
+      </p>
+
+      <UserList
+        :users="users"
+        :active-user-id="activeUserId"
+        :is-rating-tab="isRatingTab"
+        @get-user="$emit('get-user', $event)"
+      />
+
+      <div class="sidebar__footer">
+        <div class="sidebar__footer-user-info"></div>
+        <UiButton
+          title="Update list"
+          @click="$emit('update-users')"
+        />
+      </div>
     </div>
+    <div
+      v-else
+      class="sidebar__fold"
+    ></div>
   </div>
 </template>
 
